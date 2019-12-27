89f5f496a07a8fbd63b1d104b64a98284755308b
3cats <cat.katecc@gmail.com>
2019-12-27 15:13:13 -0500

    Transactions table

---
+++ src/components/Transactions/index.jsx
@@ -0,0 +1,181 @@
+import React, { PureComponent } from "react";
+import fetch from "isomorphic-unfetch";
+import FusionTable from "../FusionTable";
+import NavLink from "../NavLink";
+import TimeAgo from "../TimeAgo";
+
+export default class Transactions extends PureComponent {
+  state = {
+    loading: false
+  };
+
+  render() {
+    const { tableOptions = {} } = this.props;
+    const options = {
+      headerStyle: {
+        textAlign: "center"
+      },
+      cellStyle: {
+        textAlign: "center"
+      },
+      toolbar: false,
+      pageSize: 10,
+      pageSizeOptions: [10, 20, 50],
+      ...tableOptions
+    };
+    const style = {
+      border: "none",
+      boxShadow: "none",
+      paddingBottom: "1.75rem"
+    };
+
+    return (
+      <FusionTable
+        data={this.fetchData}
+        columns={columns}
+        options={options}
+        style={style}
+      />
+    );
+  }
+
+  toQueryString = params => {
+    return (
+      "?" +
+      Object.keys(params)
+        .map(function(key) {
+          return (
+            encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
+          );
+        })
+        .join("&")
+    );
+  };
+
+  fetchData = () =>
+    new Promise(resolve => {
+      const state = this.state;
+      if (state.loading) {
+        return;
+      }
+      this.setState({
+        ...state,
+        loading: true
+      });
+      const { params = {} } = this.props;
+      const query = this.toQueryString(params);
+      fetch(`http://localhost:8888/api/tx${query}`)
+        .then(res => res.json())
+        .then(data => {
+          this.setState({
+            loading: false
+          });
+          resolve({
+            data: data.data,
+            page: data.page - 1,
+            totalCount: data.total
+          });
+        })
+        .catch(e => {
+          this.setState({
+            loading: false
+          });
+          resolve({
+            data: [],
+            page: 1,
+            totalCount: 0
+          });
+        });
+    });
+}
+
+const columns = [
+  {
+    field: "hash",
+    title: "Tx Hash",
+    render: row => {
+      const style = {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        display: "inline-block",
+        maxWidth: "120px"
+      };
+      return (
+        <NavLink href={`/tx/${row.hash}`} className="tx-hash is-hash">
+          <span style={style}>{row.hash}</span>
+        </NavLink>
+      );
+    }
+  },
+  {
+    field: "from",
+    title: "From",
+    render: row => {
+      const style = {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        display: "inline-block",
+        maxWidth: "120px"
+      };
+      return (
+        <NavLink href={`/address/${row.from}`} className="tx-from is-hash">
+          <span style={style}>{row.from}</span>
+        </NavLink>
+      );
+    }
+  },
+  {
+    field: "to",
+    title: "To",
+    render: row => {
+      const style = {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        display: "inline-block",
+        maxWidth: "120px"
+      };
+      return (
+        <NavLink href={`/address/${row.to}`} className="tx-to is-hash">
+          <span style={style}>{row.to}</span>
+        </NavLink>
+      );
+    }
+  },
+  {
+    field: "block",
+    title: "Block",
+    render: row => {
+      return (
+        <NavLink href={`/block/${row.block}`} className="tx-block">
+          {row.block}
+        </NavLink>
+      );
+    }
+  },
+  {
+    field: "timestamp",
+    title: "Age",
+    render: row => {
+      const style = {
+        minWidth: "120px",
+        display: "inline-block"
+      };
+      return (
+        <span style={style}>
+          <TimeAgo time={row.timestamp * 1000} />
+        </span>
+      );
+    }
+  },
+  {
+    field: "type",
+    title: "Type"
+  },
+  {
+    field: "gasUsed",
+    title: "Fees"
+  }
+];
