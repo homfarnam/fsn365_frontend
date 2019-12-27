7e0c2d74118a3a443057b098983ba97b5e9d7ea1
3cats <cat.katecc@gmail.com>
2019-12-26 20:51:18 -0500

    staking page stub

--- pages/staking/index.jsx
+++ pages/staking/index.jsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { PureComponent } from 'react';
 import Router from 'next/router';
+import Head from 'next/head';
+import fetch from 'isomorphic-unfetch';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import NetworkStakingState from '../../src/components/NetworkStakingState';
+import Panel from '../../src/components/Panel';
 
-export default function StakingPage (props) {
-  return (
-    <div>
-      <h4>staking Index page</h4>
-      <Button color="primary" onClick={handleClick}>To Miner page</Button>
-      {JSON.stringify(props)}
-    </div>
-  )
-}
+const circleStyle = {
+  display: 'inline-block',
+  verticalAlign: 'center',
+  marginLeft: '10px',
+  verticalAlign: 'middle'
+};
 
-const handleClick = () => {
-  const miner = '0x47dd95c5c8ec54cc573dbbe0ab998022f3072aee';
-  Router.push(`/staking/${miner}`)
+export default class StakingPage extends PureComponent {
+  state = {
+    summary: {},
+    stakeInfo: {},
+    error: ''
+  }
+
+  async componentDidMount() {
+    const staking = await this.fetchNetworkStaingState();
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        ...staking
+      }
+    })
+  }
+
+  render () {
+    const { summary, stakeInfo, error } = this.state;
+    return (
+      <>
+        <Head><title>Fusion Miners  | FSN explorer</title></Head>
+        <Container style={{marginBottom: '1.25rem'}}>
+          <Typography variant="h4">Fusion Staking</Typography>
+        </Container>
+        <Container style={{marginBottom: '1.75rem'}}>
+          <Panel>
+            <Typography  variant="h6">Summary</Typography>
+            <div className="summary">
+              <div><strong>Total Miners:</strong> {summary.totalMiners ?  summary.totalMiners : (error ? <span>{error}</span>: <CircularProgress size={10} style={circleStyle} />)}</div>
+              <div><strong>Total Tickets:</strong> {summary.totalTickets ?  summary.totalTickets : (error ? <span>{error}</span>: <CircularProgress size={10} style={circleStyle} />)}</div>
+            </div>
+          </Panel>
+        </Container>
+        <Container>{summary.totalMiners ?
+          <NetworkStakingState data={stakeInfo} /> :
+          <Panel>
+            <Typography component="h6" variant="h6">
+              Fusion Miners {error ? <span>{error}</span>: <CircularProgress size={20} style={circleStyle} />}
+            </Typography>
+          </Panel>}
+        </Container>
+      </>
+    )
+  }
+
+  fetchNetworkStaingState = async () => {
+    return fetch('http://localhost:8888/api/staking')
+      .then(res => res.json())
+      .then(res => res.data)
+      .then((data) => {
+        return data;
+      })
+      .catch(e => {
+        return {
+          error: 'Something went wrong, please refresh page.'
+        }
+      });
+  }
 }
 
 StakingPage.getInitialProps = async({query, res}) => {
-  return {
-    isServer: !!res
+  const { miner } = query;
+  if(miner) {
+    if(res) {
+      res.writeHead(302, {
+        Location: `/staking/${miner}`
+      });
+    } else {
+      Router.push(`/staking/${miner}`);
+    }
+  } else {
+    return {
+      isServer: !!res
+    };
   }
 }
