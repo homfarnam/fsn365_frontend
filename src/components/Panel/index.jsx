cca7498d4d13be139396744aecc5095a5cd3626c
3cats <cat.katecc@gmail.com>
2019-12-26 20:39:34 -0500

    Panel component

---
+++ src/components/Panel/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  root: {
+    border: "1px solid #e7eaf3",
+    padding: "0.75rem",
+    boxShadow: "0 0.5rem 1.2rem rgba(189,197,209,.2)"
+  },
+  title: {
+    "& span": {
+      display: "inline-block",
+      borderBottom: "1px solid #e7eaf3",
+      fontWeight: "bold"
+    }
+  }
+});
+
+export default function Panel(props) {
+  const { title, children } = props;
+  const classes = useStyles();
+  return (
+    <Paper variant="outlined" className={classes.root}>
+      {/* panel title */}
+      {title ? (
+        <Typography component="h2" className={classes.title}>
+          <span>{title}</span>
+        </Typography>
+      ) : null}
+      {children}
+    </Paper>
+  );
+}
