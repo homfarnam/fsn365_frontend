4fd17daf4ad3d71879c82445942fa657b86cc449
3cats <cat.katecc@gmail.com>
2019-12-27 06:22:30 -0500

    HrSpace component

---
+++ src/components/HrSpace/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HrSpace() {
+  const style = {
+    marginTop: ".625rem",
+    marginBottom: ".625rem",
+    border: 0,
+    borderTop: "1px solid #e7eaf3"
+  };
+
+  return <hr style={style}></hr>;
+}
