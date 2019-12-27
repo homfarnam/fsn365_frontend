d82501e028b39b5228cdd50344d99a3770d24537
3cats <cat.katecc@gmail.com>
2019-12-27 06:23:37 -0500

    Extract TxField as KeyValue

--- src/components/TxOverview/index.jsx
+++ src/components/TxOverview/index.jsx
@@ -1,72 +1,29 @@
 import React from "react";
 import TimeAgo from "../TimeAgo";
 import NavLink from "../NavLink";
-import HrSpace from "../HrSpace";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
-import { useTheme } from "@material-ui/styles";
-
-const useStyles = makeStyles(({ palette }) =>
-  createStyles({
-    root: {
-      wordBreak: "break-all"
-    },
-    label: {
-      display: "inline-block",
-      marginRight: "10px",
-      verticalAlign: "center",
-      textTransform: "Capitalize"
-    },
-    value: {
-      display: "inline-block",
-      verticalAlign: "center"
-    },
-    success: {
-      color: `${palette.success}`
-    },
-    failed: {
-      color: `${palette.error}`
-    }
-  })
-);
+import KeyValue from "../KeyValue";
 
 export default function TxOverview(props) {
   const { tx } = props;
-  const theme = useTheme();
-  const classes = useStyles(theme);
   return (
-    <div className={classes.root}>
-      <TxField label="hash" value={tx.hash} />
-      <TxField label="status" value={tx.status ? "success" : "failed"} />
-      <TxField label="Block">
+    <div className="tx-overview">
+      <KeyValue label="hash" value={tx.hash} />
+      <KeyValue label="status" value={tx.status ? "success" : "failed"} />
+      <KeyValue label="Block">
         <NavLink href={`/block/${tx.block}`}>{tx.block}</NavLink>
-      </TxField>
-      <TxField label="from">
+      </KeyValue>
+      <KeyValue label="from">
         <NavLink href={`/address/${tx.from}`}>{tx.from}</NavLink>
-      </TxField>
-      <TxField label="to">
+      </KeyValue>
+      <KeyValue label="to">
         <NavLink href={`/address/${tx.to}`}>{tx.to}</NavLink>
-      </TxField>
-      <TxField label="timestamp">
+      </KeyValue>
+      <KeyValue label="timestamp">
         <span>
           <TimeAgo time={tx.timestamp * 1000}></TimeAgo>
         </span>
-      </TxField>
-      <TxField label="nonce" value={tx.nonce} />
+      </KeyValue>
+      <KeyValue label="nonce" value={tx.nonce} />
     </div>
   );
 }
-
-const TxField = props => {
-  const theme = useTheme();
-  const classes = useStyles(theme);
-  const { label, value, children } = props;
-  return (
-    <div className={`tx-${label}`}>
-      <p>
-        <span className={classes.label}>{label}:</span>
-        <span className={classes.value}>{value || children}</span>
-      </p>
-      <HrSpace />
-    </div>
-  );
-};
