1bfb8d0f93c87f6274116c691791b803f080b1de
3cats <cat.katecc@gmail.com>
2019-12-27 06:25:00 -0500

    tx detail page stub

--- pages/tx/[hash].jsx
+++ pages/tx/[hash].jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import fetch from 'isomorphic-unfetch';
+import Container from '@material-ui/core/Container';
+import Head from 'next/head';
+import Panel from '../../src/components/Panel';
+import Typography from '@material-ui/core/Typography';
+import { FusionTab, FusionTabs, FusionTabPanel } from '../../src/components/FusionTabs';
+import TxLog from '../../src/components/TxLog';
+import FusionTabPanels from '../../src/components/FusionTabs/FusionTabPanels';
+import TxOverview from '../../src/components/TxOverview';
 
-export default function TransactionDetailPage(props) {
+export default function TransactionPage(props) {
+  const { tx } = props;
+  const txLog = tx && tx.log || {};
+  const [ state, setState ] = useState({
+    tab: 0,
+  });
+  
+  const  handleTabChange = (e, newValue) => {
+    setState({
+      ...state,
+      tab: newValue
+    })
+  };
+
+  const txHasLog = Object.keys(txLog).length;
   return (
-    <div>
-      Transaction detail page
-    </div>
+    <>
+      <Head>
+        <title>Tx#{tx.type} | FSN explorer</title>
+      </Head>
+      <Container>
+        <Typography variant="h6">Tx#{tx.type}<small>({tx.coin})</small></Typography>
+        <Panel>
+          <FusionTabs
+            value={state.tab}
+            onChange={handleTabChange}
+            style={{marginBottom: '1.75rem'}}
+          >
+            <FusionTab label="Tx Overview" />
+            {txHasLog ? <FusionTab label="Tx Log" />: null}
+          </FusionTabs>
+          <FusionTabPanels>
+            <FusionTabPanel value={state.tab} index={0}>
+              <TxOverview tx={tx} />
+            </FusionTabPanel>
+            {txHasLog ? <FusionTabPanel value={state.tab} index={1}><TxLog log={txLog} /></FusionTabPanel>: null}
+          </FusionTabPanels>
+        </Panel>
+      </Container>
+    </>
   )
+}
+
+TransactionPage.getInitialProps = async ({query, res}) =>{
+  const { hash } = query;
+  const tx = await fetch(`http://localhost:8888/api/tx/${hash}`)
+    .then(res => res.json())
+    .then(res => res.data)
+    .catch(e => {});
+  return {
+    tx
+  }
 }
