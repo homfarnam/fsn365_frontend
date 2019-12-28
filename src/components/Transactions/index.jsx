1afa408edfc065052871580ddd9361371047e637
3cats <cat.katecc@gmail.com>
2019-12-27 19:45:44 -0500

    rm nouse code

--- src/components/Transactions/index.jsx
+++ src/components/Transactions/index.jsx
@@ -20,30 +20,14 @@ export default class Transactions extends PureComponent {
   render() {
     const { tableOptions = {} } = this.props;
     const options = {
-      headerStyle: {
-        textAlign: "center"
-      },
-      cellStyle: {
-        textAlign: "center"
-      },
       toolbar: false,
       pageSize: 10,
       pageSizeOptions: [10, 20, 50],
       ...tableOptions
     };
-    const style = {
-      border: "none",
-      boxShadow: "none",
-      paddingBottom: "1.75rem"
-    };
 
     return (
-      <FusionTable
-        data={this.fetchData}
-        columns={columns}
-        options={options}
-        style={style}
-      />
+      <FusionTable data={this.fetchData} columns={columns} options={options} />
     );
   }
 
@@ -101,61 +85,52 @@ const columns = [
   {
     field: "hash",
     title: "Tx Hash",
-    render: row => {
-      return (
-        <NavLink href={`/tx/${row.hash}`} className="tx-hash is-hash">
-          <span style={isHashStyle}>{row.hash}</span>
-        </NavLink>
-      );
-    }
+    render: row => (
+      <NavLink href={`/tx/${row.hash}`}>
+        <span style={isHashStyle}>{row.hash}</span>
+      </NavLink>
+    )
   },
   {
     field: "from",
     title: "From",
-    render: row => {
-      return (
-        <NavLink href={`/address/${row.from}`} className="tx-from is-hash">
-          <span style={isHashStyle}>{row.from}</span>
-        </NavLink>
-      );
-    }
+    render: row => (
+      <NavLink href={`/address/${row.from}`}>
+        <span style={isHashStyle}>{row.from}</span>
+      </NavLink>
+    )
   },
   {
     field: "to",
     title: "To",
-    render: row => {
-      return (
-        <NavLink href={`/address/${row.to}`} className="tx-to is-hash">
-          <span style={isHashStyle}>{row.to}</span>
-        </NavLink>
-      );
-    }
+    render: row => (
+      <NavLink href={`/address/${row.to}`}>
+        <span style={isHashStyle}>{row.to}</span>
+      </NavLink>
+    )
   },
   {
     field: "block",
     title: "Block",
-    render: row => {
-      return (
-        <NavLink href={`/block/${row.block}`} className="tx-block">
-          {row.block}
-        </NavLink>
-      );
-    }
+    render: row => (
+      <NavLink href={`/block/${row.block}`} className="tx-block">
+        {row.block}
+      </NavLink>
+    )
   },
   {
     field: "timestamp",
     title: "Age",
-    render: row => {
-      const style = {
-        minWidth: "120px",
-        display: "inline-block"
-      };
-      return (
-        <span style={style}>
-          <TimeAgo time={row.timestamp * 1000} />
-        </span>
-      );
-    }
+    render: row => (
+      <span
+        style={{
+          minWidth: "120px",
+          display: "inline-block"
+        }}
+      >
+        <TimeAgo time={row.timestamp * 1000} />
+      </span>
+    )
   },
   {
     field: "type",
