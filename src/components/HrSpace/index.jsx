ede93581d446871c1707541f1b78f32b2232de09
3cats <cat.katecc@gmail.com>
2019-12-28 14:53:26 -0500

    add more space for hrSpace

--- src/components/HrSpace/index.jsx
+++ src/components/HrSpace/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HrSpace() {
   const style = {
-    marginTop: ".625rem",
-    marginBottom: ".625rem",
+    marginTop: ".75rem",
+    marginBottom: ".75rem",
     border: 0,
     borderTop: "1px solid #e7eaf3"
   };
