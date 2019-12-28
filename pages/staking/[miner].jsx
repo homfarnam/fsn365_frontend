7c77525c296cf3d08e79e862bed95885adb68eb3
3cats <cat.katecc@gmail.com>
2019-12-28 13:35:46 -0500

    Mining Overview for a mining node

--- pages/staking/[miner].jsx
+++ pages/staking/[miner].jsx
@@ -2,40 +2,28 @@ import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
 import Head from 'next/head';
 import Typography from '@material-ui/core/Typography';
-import Panel from '../../src/components/Panel';
 import MiningState from '../../src/components/MiningState';
+import MiningOverview from '../../src/components/MiningOverview';
 
-export default class MinerStakingPage extends Component {
-  state = {};
-
-  render() {
-    const { miner, tab = 'block' } = this.props;
-    return (
-      <>
-        <Head><title>Node Monitor | FSN explorer</title></Head>
-        <Container style={{marginBottom: '1.25rem'}}>
-          <Typography variant="h4">Node Monitor</Typography>
-        </Container>
-        <Container style={{marginBottom: '1.75rem'}}>
-          <Panel>
-            <Typography variant="h6"><span>Overview</span></Typography>
-          </Panel>
-        </Container>
-        <Container>
-          <MiningState miner={miner} view={tab} />
-        </Container>
-      </>
-    )
-  }
-
-  handleChange = (e, newValue) => {
-    console.log(e);
-  }
+export default function MinerStakingPage ({miner, tab="block"}) {
+  return (
+    <>
+      <Head><title>Node Monitor | FSN explorer</title></Head>
+      <Container style={{marginBottom: '1.25rem'}}>
+        <Typography variant="h4">Node Monitor</Typography>
+      </Container>
+      <Container style={{marginBottom: '1.75rem'}}>
+        <MiningOverview miner={miner} />
+      </Container>
+      <Container>
+        <MiningState miner={miner} view={tab} />
+      </Container>
+    </>
+  )
 }
 
 MinerStakingPage.getInitialProps = async ({query, res}) =>{
   return {
     ...query,
-    isServer: !!res
   }
 }
---
+++ src/components/MiningOverview/index.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import fetch from "isomorphic-unfetch";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Box from "@material-ui/core/Box";
+import KeyValue from "../KeyValue";
+import Panel from "../Panel";
+import TimeAgo from "../TimeAgo";
+
+const useStyles = makeStyles(({ breakpoints }) =>
+  createStyles({
+    overview: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-between",
+      break: "break-all"
+    },
+    field: {
+      width: "100%",
+      [breakpoints.up("lg")]: {
+        width: "49%"
+      }
+    }
+  })
+);
+
+export default function MiningOverview({ miner }) {
+  const [state, setState] = useState({
+    msg: "loading",
+    overview: {}
+  });
+  useEffect(() => {
+    fetch(`http://localhost:8888/api/address/${miner}/staking`)
+      .then(res => res.json())
+      .then(res => ({ overview: res.data, msg: "" }))
+      .catch(e => ({
+        msg: "Something went wrong, please refresh page!",
+        overview: {}
+      }))
+      .then(data => {
+        setState(data);
+      });
+  }, [miner]);
+  const { msg, overview } = state;
+  const classes = useStyles();
+  return (
+    <Panel title="Overview">
+      {msg == "loading" ? (
+        <CircularProgress size={20} />
+      ) : (
+        <Box color="error.main" component="strong">
+          {msg}
+        </Box>
+      )}
+      {!msg && (
+        <div className={classes.overview}>
+          <KeyValue label="Address" value={overview.address} />
+          <KeyValue label="Node Status" className={classes.field}>
+            {overview.tickets ? (
+              <Box color="success.main" component="strong">
+                Online
+              </Box>
+            ) : (
+              <Box color="error.main" component="strong">
+                Offline
+              </Box>
+            )}
+          </KeyValue>
+          {overview.tickets ? (
+            <KeyValue
+              label="Tickets"
+              value={overview.tickets}
+              className={classes.field}
+            />
+          ) : null}
+          <KeyValue
+            label="Rewards To Date"
+            value={overview.rewards + "FSN"}
+            className={classes.field}
+          />
+          <KeyValue label="Last Mined Block" className={classes.field}>
+            <TimeAgo time={overview.latestMinedTime * 1000} />
+          </KeyValue>
+        </div>
+      )}
+    </Panel>
+  );
+}
