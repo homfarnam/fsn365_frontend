2822b6ee8924ffbe5faf3000e9b6cce18ad8e423
3cats <cat.katecc@gmail.com>
2019-12-27 20:00:13 -0500

    rm nouse code

--- src/components/AddressAssets/index.jsx
+++ src/components/AddressAssets/index.jsx
@@ -2,33 +2,14 @@ import React from "react";
 import FusionTable from "../FusionTable";
 import NavLink from "../NavLink";
 
-export default function AddressHeldAssets(props) {
-  const { assets } = props;
-  const style = {
-    border: "none",
-    boxShadow: "none",
-    paddingBottom: "1.75rem"
-  };
-  const tableOptions = {
-    headerStyle: {
-      textAlign: "center"
-    },
-    cellStyle: {
-      textAlign: "center"
-    },
-    toolbar: false,
-    pageSize: 10,
-    paging: false,
-    pageSizeOptions: [5, 10]
-  };
-  return (
-    <FusionTable
-      columns={columns}
-      data={assets}
-      style={style}
-      options={tableOptions}
-    />
-  );
+const tableOptions = {
+  toolbar: false,
+  pageSize: 10,
+  paging: false,
+  pageSizeOptions: [5, 10]
+};
+export default function AddressHeldAssets({ assets }) {
+  return <FusionTable columns={columns} data={assets} options={tableOptions} />;
 }
 
 const columns = [
@@ -36,39 +17,28 @@ const columns = [
     field: "name",
     title: "Asset Name",
     sorting: false,
-    render: row => {
-      const href = `/assset/${row.id}`;
-      return (
-        <NavLink href={href} className="asset-name">
-          {row.name}{" "}
-        </NavLink>
-      );
-    }
+    render: row => <NavLink href={`/assset/${row.id}`}>{row.name}</NavLink>
   },
   {
     field: "symbol",
     title: "Asset Symbol",
     sorting: false,
-    render: row => {
-      return <span className="asset-symbol">{row.symbol}</span>;
-    }
+    render: row => <span className="asset-symbol">{row.symbol}</span>
   },
   {
     field: "verified",
     title: "Is Verified",
     sorting: false,
-    render: row => {
-      return (
-        <span className="asset-symbol">{row.verified ? "Yes" : "No"}</span>
-      );
-    }
+    render: row => (
+      <span className="asset-symbol">{row.verified ? "Yes" : "No"}</span>
+    )
   },
   {
     field: "quantity",
     title: "Quantity",
     sorting: false,
-    render: row => {
-      return <span className="asset-quantity">{row.quantity.toFixed(2)}</span>;
-    }
+    render: row => (
+      <span className="asset-quantity">{row.quantity.toFixed(2)}</span>
+    )
   }
 ];
--- src/components/AddressLockedAssets/index.jsx
+++ src/components/AddressLockedAssets/index.jsx
@@ -3,32 +3,14 @@ import FusionTable from "../FusionTable";
 import NavLink from "../NavLink";
 import TimeAgo from "../TimeAgo";
 
-export default function AddressHeldAssets(props) {
-  const { assets } = props;
-  const style = {
-    border: "none",
-    boxShadow: "none",
-    paddingBottom: "1.75rem"
-  };
-  const tableOptions = {
-    headerStyle: {
-      textAlign: "center"
-    },
-    cellStyle: {
-      textAlign: "center"
-    },
-    toolbar: false,
-    pageSize: 5,
-    pageSizeOptions: [5, 10]
-  };
-  return (
-    <FusionTable
-      columns={columns}
-      data={assets}
-      style={style}
-      options={tableOptions}
-    />
-  );
+const tableOptions = {
+  toolbar: false,
+  pageSize: 5,
+  pageSizeOptions: [5, 10]
+};
+
+export default function AddressHeldAssets({ assets }) {
+  return <FusionTable columns={columns} data={assets} options={tableOptions} />;
 }
 
 const columns = [
@@ -36,45 +18,38 @@ const columns = [
     field: "name",
     title: "Asset",
     sorting: false,
-    render: row => {
-      const href = `/assset/${row.id}`;
-      return (
-        <NavLink href={href} className="a-na">
-          {row.name} ({row.symbol})
-        </NavLink>
-      );
-    }
+    render: row => (
+      <NavLink href={`/assset/${row.id}`} className="a-na">
+        {row.name} ({row.symbol})
+      </NavLink>
+    )
   },
   {
     field: "startTime",
     title: "Locked At",
     sorting: false,
-    render: row => {
-      return (
-        <span className="a-lockedAt">
-          <TimeAgo time={row.startTime} />
-        </span>
-      );
-    }
+    render: row => (
+      <span className="a-lockedAt">
+        <TimeAgo time={row.startTime} />
+      </span>
+    )
   },
   {
     field: "endTime",
     title: "Will Unlock At",
     sorting: false,
-    render: row => {
-      return (
-        <span className="a-symbol">
-          <TimeAgo time={row.endTime} />
-        </span>
-      );
-    }
+    render: row => (
+      <span className="a-symbol">
+        <TimeAgo time={row.endTime} />
+      </span>
+    )
   },
   {
     field: "quantity",
     title: "Quantity",
     sorting: false,
-    render: row => {
-      return <span className="asset-quantity">{row.quantity.toFixed(2)}</span>;
-    }
+    render: row => (
+      <span className="asset-quantity">{row.quantity.toFixed(2)}</span>
+    )
   }
 ];
