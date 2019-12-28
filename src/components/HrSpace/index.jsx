95953a8ac606f5d404d51780a8d93f41d1d0441c
3cats <cat.katecc@gmail.com>
2019-12-28 14:59:27 -0500

    mv obj style into class

--- src/components/HrSpace/index.jsx
+++ src/components/HrSpace/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(({ palette }) =>
+  createStyles({
+    root: {
+      marginTop: ".75rem",
+      marginBottom: ".75rem",
+      border: 0,
+      borderTop: "1px solid #e7eaf3"
+    }
+  })
+);
 
 export default function HrSpace() {
-  const style = {
-    marginTop: ".75rem",
-    marginBottom: ".75rem",
-    border: 0,
-    borderTop: "1px solid #e7eaf3"
-  };
-
-  return <hr style={style}></hr>;
+  const classes = useStyles();
+  return <hr class={classes.root}></hr>;
 }
