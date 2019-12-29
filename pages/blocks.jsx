877e6093030c1ffe82bd3c669c64222ce0dcc615
3cats <cat.katecc@gmail.com>
2019-12-29 10:10:42 -0500

    support miner query

--- pages/blocks.jsx
+++ pages/blocks.jsx
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import Container from '@material-ui/core/Container';
 import Panel from '../src/components/Panel';
 import Typography from '@material-ui/core/Typography';
 import MinedBlocks from '../src/components/MinedBlocks';
 
+export default function BlockListPage (props) {
+  const {miner} = props;
+  return (
+    <>
+      <Head>
+        <title>Blocks | FSN explorer</title>
+      </Head>
+      <Container>
+        <Typography variant='h6'>Blocks</Typography>
+        <Panel>
+          <MinedBlocks
+            miner={miner}
+            tableOptions={{pageSize: 10, pageSizeOptions: [10,20,50]}}
+          />
+        </Panel>
+      </Container>
+    </>
+  )
+}
 
-export default class BlockListPage extends Component {
-  render () {
-    const {miner} = this.props;
-    return (
-      <>
-        <Head>
-          <title>Blocks | FSN explorer</title>
-        </Head>
-        <Container>
-          <Typography variant='h6'>Blocks</Typography>
-          <Panel>
-            <MinedBlocks
-              miner={miner}
-              tableOptions={{pageSize: 10, pageSizeOptions: [10,20,50]}}
-            />
-          </Panel>
-        </Container>
-      </>
-    )
+BlockListPage.getInitialProps = async({query}) => {
+  return {
+    ...query
   }
 }
