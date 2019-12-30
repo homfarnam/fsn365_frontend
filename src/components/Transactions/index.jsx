7aae768307486500a5af43e86403a757f3e6dc3b
3cats <cat.katecc@gmail.com>
2019-12-30 18:30:51 -0500

    refactor transactions.index.jsx into pure function

--- src/components/Transactions/index.jsx
+++ src/components/Transactions/index.jsx
@@ -1,125 +1,118 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import FusionTable from "../FusionTable";
 import fetch from "../../libs/fetch";
 import NavLink from "../NavLink";
 import TimeAgo from "../TimeAgo";
 import FusionAdressLink from "../FusionAdressLink";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
-export default class Transactions extends PureComponent {
-  render() {
-    const { tableOptions = {} } = this.props;
-    const options = {
-      toolbar: false,
-      pageSize: 10,
-      ...tableOptions
-    };
-
-    return (
-      <FusionTable data={this.fetchData} columns={columns} options={options} />
-    );
-  }
-
-  toQueryString = params => {
-    return (
-      "?" +
-      Object.keys(params)
-        .map(function(key) {
-          return (
-            encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
-          );
-        })
-        .join("&")
-    );
+export default function Transactions(props) {
+  const { tableOptions = {}, params = {} } = props;
+  const options = {
+    toolbar: false,
+    pageSize: 10,
+    ...tableOptions
   };
-
-  fetchData = ({ page, pageSize }) =>
-    new Promise(resolve => {
-      const { params = {} } = this.props;
-      params.page = page + 1;
-      params.size = pageSize;
-      const query = this.toQueryString(params);
-      fetch(`/tx${query}`)
-        .then(res => res.json())
-        .then(data => {
-          resolve({
-            data: data.data,
-            page: data.page - 1,
-            totalCount: data.total
-          });
-        })
-        .catch(e => {
-          resolve({
-            data: [],
-            page: 1,
-            totalCount: 0
-          });
-        });
-    });
+  const queryDataFunc = createQuery(params);
+  const columns = createColumns();
+  return (
+    <FusionTable data={queryDataFunc} columns={columns} options={options} />
+  );
 }
 
-const isHashStyle = {
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-  whiteSpace: "nowrap",
-  display: "inline-block",
-  maxWidth: "120px"
+const createQuery = params => ({ page, pageSize }) => {
+  return new Promise(resolve => {
+    params.page = page + 1;
+    params.size = pageSize;
+    fetch(`/tx`, params)
+      .then(res => res.json())
+      .then(data => {
+        resolve({
+          data: data.data,
+          page: data.page - 1,
+          totalCount: data.total
+        });
+      })
+      .catch(e => {
+        resolve({
+          data: [],
+          page: 1,
+          totalCount: 0
+        });
+      });
+  });
 };
 
-const columns = [
-  {
-    field: "hash",
-    title: "Tx Hash",
-    sorting: false,
-    render: row => (
-      <NavLink href={`/tx/${row.hash}`} prefetch={false}>
-        <span style={isHashStyle}>{row.hash}</span>
-      </NavLink>
-    )
-  },
-  {
-    field: "from",
-    title: "From",
-    sorting: false,
-    render: row => <FusionAdressLink address={row.from} style={isHashStyle} />
-  },
-  {
-    field: "to",
-    title: "To",
-    render: row => <FusionAdressLink address={row.to} style={isHashStyle} />
-  },
-  {
-    field: "block",
-    title: "Block",
-    sorting: false,
-    render: row => (
-      <NavLink href={`/block/${row.block}`} className="tx-block">
-        {row.block}
-      </NavLink>
-    )
-  },
-  {
-    field: "timestamp",
-    title: "Age",
-    sorting: false,
-    render: row => (
-      <span
-        style={{
-          minWidth: "120px",
-          display: "inline-block"
-        }}
-      >
-        <TimeAgo time={row.timestamp * 1000} />
-      </span>
-    )
-  },
-  {
-    field: "type",
-    title: "Tx Type",
-    sorting: false
-  },
-  {
-    field: "gasUsed",
-    title: "Fees",
-    sorting: false
-  }
-];
+const useStyles = makeStyles(({ palette }) =>
+  createStyles({
+    isHash: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+      display: "inline-block",
+      maxWidth: "120px"
+    },
+    timestamp: {
+      minWidth: "120px",
+      display: "inline-block"
+    }
+  })
+);
+
+const createColumns = () => {
+  const classes = useStyles();
+  return [
+    {
+      field: "hash",
+      title: "Tx Hash",
+      sorting: false,
+      render: row => (
+        <NavLink href={`/tx/${row.hash}`} prefetch={false}>
+          <span className={classes.isHash}>{row.hash}</span>
+        </NavLink>
+      )
+    },
+    {
+      field: "from",
+      title: "From",
+      sorting: false,
+      render: row => (
+        <FusionAdressLink address={row.from} className={classes.isHash} />
+      )
+    },
+    {
+      field: "to",
+      title: "To",
+      sorting: false,
+      render: row => (
+        <FusionAdressLink address={row.to} className={classes.isHash} />
+      )
+    },
+    {
+      field: "block",
+      title: "Block",
+      sorting: false,
+      render: row => <NavLink href={`/block/${row.block}`}>{row.block}</NavLink>
+    },
+    {
+      field: "timestamp",
+      title: "Age",
+      sorting: false,
+      render: row => (
+        <span className={classes.timestamp}>
+          <TimeAgo time={row.timestamp * 1000} />
+        </span>
+      )
+    },
+    {
+      field: "type",
+      title: "Tx Type",
+      sorting: false
+    },
+    {
+      field: "gasUsed",
+      title: "Fees",
+      sorting: false
+    }
+  ];
+};
