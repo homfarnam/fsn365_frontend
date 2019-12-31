fa805881a2010eb8a461bf54a872024995f45a59
3cats <cat.katecc@gmail.com>
2019-12-31 17:25:34 -0500

    add prod api link

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-const isProd = process.env.NODE_ENV === 'production' ? true: false;
+const isProd = process.env.NODE_ENV == 'production' ? true: false;
 
 module.exports ={
   publicRuntimeConfig: {
-    API_PATH: isProd ? 'http://localhost:8888/api': 'http://localhost:8888/api',
-  }
+    API_PATH: isProd ? 'http://localhost:8888/api': 'http://47.103.55.126/api',
+  },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
+  devIndicators: {
+    autoPrerender: false,
+  },
+  compress: false,
 };
