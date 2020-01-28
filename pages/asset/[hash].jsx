da53f16a3ae59269ffab44b90051951ec9b99c9b
3cats <cat.katecc@gmail.com>
2020-01-28 10:36:50 -0500

    atune to api

--- pages/asset/[hash].jsx
+++ pages/asset/[hash].jsx
@@ -1,41 +1,55 @@
-import React  from 'react';
-import Panel from '../../src/components/Panel';
-import KeyValue from '../../src/components/KeyValue';
-import Box from '@material-ui/core/Box';
-import PageHeading from '../../src/components/PageHeading';
+import React from "react";
+import Panel from "../../src/components/Panel";
+import KeyValue from "../../src/components/KeyValue";
+import Box from "@material-ui/core/Box";
+import PageHeading from "../../src/components/PageHeading";
 import FusionAddressLink from "../../src/components/FusionAddressLink";
-import fetch from '../../src/libs/fetch';
+import fetch from "../../src/libs/fetch";
 
-export default function AssetPage ({asset}) {
+export default function AssetPage({ asset }) {
   return (
     <>
-      <PageHeading title={'Asset'} suffix={`#${asset.symbol}`} />
-        <Panel>
-          <KeyValue label="Asset Name" value={asset.name} />
-          <KeyValue label="Asset symbol" value={asset.symbol} />
-          <KeyValue label="Asset ID" value={asset.id} />
-          <KeyValue label="Decimals" value={asset.decimals} />
-          <KeyValue label="Total Supply" value={+asset.total/Math.pow(10, asset.decimals)} />
-          <KeyValue label="Changeable Supply" value={asset.changeable ? 'Yes': 'No'} />
-          <KeyValue label="Issuer">
-            <FusionAddressLink address={asset.issuer} />
-          </KeyValue>
-          <KeyValue label="Issue Height" value={asset.block} />
-          <KeyValue label="Verified">
-            {asset.verified ? <Box color="success.main" component="strong">Yes</Box> : <Box color="error.main" component="strong">No</Box>}
-          </KeyValue>
-        </Panel>
+      <PageHeading title={"Asset"} suffix={`#${asset.symbol}`} />
+      <Panel>
+        <KeyValue label="Asset Name" value={asset.name} />
+        <KeyValue label="Asset symbol" value={asset.symbol} />
+        <KeyValue label="Asset ID" value={asset.id} />
+        <KeyValue label="Decimals" value={asset.decimals} />
+        <KeyValue
+          label="Total Supply"
+          value={+asset.total / Math.pow(10, asset.decimals)}
+        />
+        <KeyValue
+          label="Changeable Supply"
+          value={asset.changeable ? "Yes" : "No"}
+        />
+        <KeyValue label="Issuer">
+          <FusionAddressLink address={asset.issuer} />
+        </KeyValue>
+        <KeyValue label="Issue Height" value={asset.height} />
+        <KeyValue label="Verified">
+          {asset.verified ? (
+            <Box color="success.main" component="strong">
+              Yes
+            </Box>
+          ) : (
+            <Box color="error.main" component="strong">
+              No
+            </Box>
+          )}
+        </KeyValue>
+      </Panel>
     </>
-  )
+  );
 }
 
-AssetPage.getInitialProps = async ({query}) =>{
+AssetPage.getInitialProps = async ({ query }) => {
   const { hash } = query;
   const asset = await fetch(`/asset/${hash}`)
     .then(res => res.json())
     .then(res => res.data)
-    .catch(e => ({hash}));
+    .catch(e => ({ hash }));
   return {
     asset
-  }
-}
+  };
+};
