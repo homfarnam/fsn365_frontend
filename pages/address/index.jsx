f0a7cd9761c34e9a30f1ea3fb50154e3d26d664d
3cats <cat.katecc@gmail.com>
2020-01-29 12:11:59 -0500

    rm staking rewards

--- pages/address/index.jsx
+++ pages/address/index.jsx
@@ -1,60 +1,75 @@
-import React  from 'react';
-import FusionTable from '../../src/components/FusionTable';
-import NavLink from '../../src/components/NavLink';
-import Panel from '../../src/components/Panel';
-import PageHeading from '../../src/components/PageHeading';
-import fetch from '../../src/libs/fetch';
+import React from "react";
+import FusionTable from "../../src/components/FusionTable";
+import NavLink from "../../src/components/NavLink";
+import Panel from "../../src/components/Panel";
+import PageHeading from "../../src/components/PageHeading";
+import fetch from "../../src/libs/fetch";
+import TimeAgo from '../../src/components/TimeAgo';
 
-export default function AddressListPage () {
+export default function AddressListPage() {
   return (
     <>
-      <PageHeading title="Adresses" />
+      <PageHeading title="Fusion Address" />
       <Panel>
+        <p><strong>Notice:</strong>We only list address that holds more than one FSN in wallet at this page.</p>
         <FusionTable
-          data = {fetchData}
+          data={fetchData}
           columns={columns}
-          options={{toolbar:false}}
+          options={{ toolbar: false }}
         />
       </Panel>
     </>
-  )
+  );
 }
-const fetchData =  ({page, pageSize}) => new Promise((resolve) => {
-  const query = `?page=${page+1}&size=${pageSize}`;
-  fetch(`/address${query}`)
-  .then(res => res.json())
-  .then((data) => {
-    resolve({
-      data: data.data,
-      page: data.page - 1 ,
-      totalCount: data.total
-    })
-  })
-  .catch(e => {
-    resolve({
-      data: [],
-      page: 0,
-      totalCount: 0
-    })
+const fetchData = query =>
+  new Promise(resolve => {
+    const {
+      page = 1,
+      pageSize = 10,
+      orderDirection = "desc"
+    } = query;
+    const params = {
+      page: page + 1,
+      size: pageSize,
+      order: orderDirection || "desc",
+      sort: "fsnBalance"
+    };
+    fetch(`/address`, params)
+      .then(res => res.json())
+      .then(data => {
+        resolve({
+          data: data.data,
+          page: page,
+          totalCount: data.total
+        });
+      })
+      .catch(e => {
+        resolve({
+          data: [],
+          page: 0,
+          totalCount: 0
+        });
+      });
   });
-})
 
 const columns = [
   {
     field: "address",
     title: "Address",
     sorting: false,
-    render: row => <NavLink href={`/address/${row.address}`}>{row.address}</NavLink>
+    render: row => (
+      <NavLink href={`/address/${row.address}`}>{row.address}</NavLink>
+    )
   },
   {
     field: "san",
     title: "SAN",
-    sorting: false,
+    sorting: false
   },
   {
     filed: "fsnBalance",
     title: "FSN Balance",
-    render: row  => <span>{row.fsnBalance}</span>
+    render: row => <span>{row.fsnBalance}</span>
   },
   {
     field: "transactions",
@@ -63,9 +78,9 @@ const columns = [
     render: row => <span>{row.txCount}</span>
   },
   {
-    field: "stakingRewards",
-    title: "Staking Rewards",
+    field: 'latestActiveTime',
+    title: 'Latest Active Time',
     sorting: false,
-    render: row =>  <span >{row.rewards}</span>
+    render: row => <TimeAgo time={row.latestActiveTime * 1000}  />
   }
 ];
