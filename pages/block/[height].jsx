c3354aa984514f7ae326a4d3be25df561568cc6d
3cats <cat.katecc@gmail.com>
2020-01-29 21:10:29 -0500

    format code

--- pages/block/[height].jsx
+++ pages/block/[height].jsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
-import Router from 'next/router';
-import Panel from '../../src/components/Panel';
-import { FusionTab, FusionTabs, FusionTabPanel } from '../../src/components/FusionTabs';
-import FusionTabPanels from '../../src/components/FusionTabs/FusionTabPanels';
-import BlockOverview from '../../src/components/BlockOverview';
-import TxsInBlock from '../../src/components/TxsInBlock';
-import PageHeading from '../../src/components/PageHeading';
-import fetch from '../../src/libs/fetch';
+import React, { useState } from "react";
+import Router from "next/router";
+import Panel from "../../src/components/Panel";
+import {
+  FusionTab,
+  FusionTabs,
+  FusionTabPanel
+} from "../../src/components/FusionTabs";
+import FusionTabPanels from "../../src/components/FusionTabs/FusionTabPanels";
+import BlockOverview from "../../src/components/BlockOverview";
+import TxsInBlock from "../../src/components/TxsInBlock";
+import PageHeading from "../../src/components/PageHeading";
+import fetch from "../../src/libs/fetch";
 
 const tabMap = {
-  'overview': 0,
-  'tx': 1
+  overview: 0,
+  tx: 1
 };
 
-export default function BlockPage (props) {
-  const { block, tab = "overview" } = props;
-  const [ state, setState ] = useState({
-    tab: tabMap[tab] || 0,
+export default function BlockPage(props) {
+  const { block = {}, tab = "overview" } = props;
+  const [state, setState] = useState({
+    tab: tabMap[tab] || 0
   });
-  
-  const  handleTabChange = (e, newValue) => {
+
+  const handleTabChange = (e, newValue) => {
     setState({
       ...state,
       tab: newValue
@@ -29,43 +33,50 @@ export default function BlockPage (props) {
   const hasTx = block.txCount > 0;
   return (
     <>
-      <PageHeading title={'Block'} suffix={`#${block.height}`} />
+      <PageHeading title={"Block"} suffix={`#${block.height}`} />
       <Panel>
-        <FusionTabs
-          value={state.tab}
-          onChange={handleTabChange}
-        >
-          <FusionTab label="overview"  />
-          {hasTx ? <FusionTab label="Txs" />:null}
+        <FusionTabs value={state.tab} onChange={handleTabChange}>
+          <FusionTab label="overview" />
+          {hasTx ? <FusionTab label="Txs" /> : null}
         </FusionTabs>
         <FusionTabPanels>
-          <FusionTabPanel value={state.tab} index={0}><BlockOverview block={block} /></FusionTabPanel>
-          {hasTx ? <FusionTabPanel value={state.tab} index={1} style={{marginTop: '-1rem'}}><TxsInBlock block={block.height} /></FusionTabPanel>:null}
+          <FusionTabPanel value={state.tab} index={0}>
+            <BlockOverview block={block} />
+          </FusionTabPanel>
+          {hasTx ? (
+            <FusionTabPanel
+              value={state.tab}
+              index={1}
+              style={{ marginTop: "-1rem" }}
+            >
+              <TxsInBlock block={block.height} />
+            </FusionTabPanel>
+          ) : null}
         </FusionTabPanels>
       </Panel>
     </>
-  )
+  );
 }
 
-BlockPage.getInitialProps = async ({query, res}) =>{
+BlockPage.getInitialProps = async ({ query, res }) => {
   const height = Math.abs(Number(query.height));
-  if(isNaN(height)) {
-    if(res) {
+  if (isNaN(height)) {
+    if (res) {
       res.writeHead(302, {
-        Location: '/blocks'
+        Location: "/blocks"
       });
       res.end();
     } else {
-      Router.push('/blocks')
+      Router.push("/blocks");
     }
   } else {
-     const block = await fetch(`/block/${height}`)
+    const block = await fetch(`/block/${height}`)
       .then(res => res.json())
       .then(res => res.data)
       .catch(e => {});
     return {
       block,
-      ...query,
-    }
+      ...query
+    };
   }
-}
+};
