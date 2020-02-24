eb82f5e914bd76f45e3ac67acfa40f51b526c2af
3cats <cat.katecc@gmail.com>
2020-02-24 13:15:43 -0500

    replace timeago.js with React TiemAgo component

--- src/components/TimeAgo/index.jsx
+++ src/components/TimeAgo/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { format } from "timeago.js";
 import PropType from "prop-types";
+import TimeAgo from "react-timeago";
 
-export default function TimeAgo(props) {
+export default function InnerTimeAgo(props) {
   const { time } = props;
-  return <>{format(time)}</>;
+  return <TimeAgo date={time} formatter={formatter}></TimeAgo>;
 }
 
 TimeAgo.propTypes = {
   time: PropType.number
 };
+
+function formatter() {
+  const value = arguments[0];
+  let desc = arguments[1];
+  const suffix = arguments[2];
+
+  switch (desc) {
+    case "second":
+      desc = "secs";
+      break;
+    case "minute":
+      desc = value > 1 ? "mins" : "min";
+      break;
+    default:
+      break;
+  }
+  return `${value} ${desc} ${suffix}`;
+}
