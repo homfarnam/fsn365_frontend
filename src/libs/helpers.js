ff6a836ae66b7aa27874336b7036356bd8f596ab
3cats <cat.katecc@gmail.com>
2020-03-05 15:21:36 -0500

    lowercase k

--- src/libs/helpers.js
+++ src/libs/helpers.js
@@ -3,7 +3,7 @@ export function formatValue(rawValue) {
   if (rawValue > 1000000) {
     value = (rawValue / Math.pow(10, 6)).toFixed(2) + "M";
   } else if (rawValue >= 1000) {
-    value = (rawValue / Math.pow(10, 3)).toFixed(2) + "K";
+    value = (rawValue / Math.pow(10, 3)).toFixed(2) + "k";
   } else if (rawValue >= 0.55) {
     value = rawValue.toFixed(2);
   } else {
