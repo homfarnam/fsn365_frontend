0dbde7e09a2eb66674c67ec800847d4d232f1dc2
3cats <cat.katecc@gmail.com>
2020-03-08 12:40:41 -0400

    update labeld address

--- src/constants/addressMap.js
+++ src/constants/addressMap.js
@@ -24,5 +24,15 @@ export default {
   "0xac77a273b4df219912edac0a3e08c9d8e6452b8a": "UPBIT",
   "0x9996c35cdb692de8a76397e83b9a30982f1b0692": "BITTREX",
   "0x23cfd2a6c890d7904289ba28526f019607628f03": "FOUNDATION",
-  "0x83a7c259e944a21dcad78d328c238b8147db87e0": "BIBOX"
+  "0x83a7c259e944a21dcad78d328c238b8147db87e0": "BIBOX",
+  "0x9c48c796cb0bed51a14291bc8cc56dab5aed7b5c": "Foundation",
+  "0x0Ac469cf34C4d65515E4eF27eAF0F71F15C5A2fd": "Foundation",
+  "0xd2452651834e8f0c19c9d85e0bf09fe99283dabc": "Foundation Miner",
+  "0x7da669ea4234473e224a8f5a5f6257729e6f03cd": "Foundation Miner",
+  "0x8e448859d7502b1ecf0c8d18008feeff89a30c60": "Foundation Miner",
+  "0x722c6716dad0d7f8c1adbfd3360edbe99813f3aa": "Foundation Miner",
+  "0x0e93335afe434a73e005cab8bd1b2dc153b73549": "Foundation Miner",
+  "0xe3db01c89dcdd68b80e7dfdf931b00a6335d72dd": "Foundation Miner",
+  "0xc4a9441afb052cb454240136cce71fb09316ea94": "Foundation Miner",
+  "0xfa37b7c3f21060458361ed5322be5af3740bce3c": "Foundation Miner"
 };
