eb01393d37e934b4555156ac7ddb2349bac0128a
3cats <cat.katecc@gmail.com>
2020-04-02 18:56:16 -0400

    limit 1k

--- pages/blocks.jsx
+++ pages/blocks.jsx
@@ -1,44 +1,55 @@
-import React from 'react';
-import Panel from '../src/components/Panel';
-import PageHeading from '../src/components/PageHeading';
-import MinedBlocks from '../src/components/MinedBlocks';
-import { makeStyles, createStyles,  } from "@material-ui/core/styles";
-import FusionAddressLink from '../src/components/FusionAddressLink';
+import React from "react";
+import Panel from "../src/components/Panel";
+import PageHeading from "../src/components/PageHeading";
+import MinedBlocks from "../src/components/MinedBlocks";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import FusionAddressLink from "../src/components/FusionAddressLink";
+import TextStrong from "../src/components/TextStrong";
 
 const useStyles = makeStyles(() =>
   createStyles({
     address: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      wordBreak: 'break-all',
-      paddingLeft: '.75rem'
+      display: "flex",
+      flexWrap: "wrap",
+      wordBreak: "break-all",
+      paddingLeft: ".75rem",
     },
     span: {
-      margin: '0 4px',
-      display: 'inline-block'
+      margin: "0 4px",
+      display: "inline-block",
     },
   })
 );
 
-
-export default function BlockListPage ({params}) {
+export default function BlockListPage({ params }) {
   const classes = useStyles();
   return (
     <>
-      <PageHeading title={'Blocks'} />
-        <Panel style={{paddingTop:0, paddingLeft:0, paddingRight:0}}>
-          {params.miner ? <p className={classes.address}><span className={classes.span}>Blocks mined by</span> <FusionAddressLink address={params.miner} /></p>:null}
-          <MinedBlocks
-            miner={params.miner}
-            tableOptions={{pageSize: 10, pageSizeOptions: [10,20,50]}}
-          />
-        </Panel>
+      <PageHeading title={"Blocks"} />
+      <Panel style={{ paddingTop: 0, paddingLeft: 0, paddingRight: 0 }}>
+        {params.miner ? (
+          <p p className={classes.address}>
+            <TextStrong>Notice:</TextStrong>We only provide latest 1k blocks
+            mined by miner.
+          </p>
+        ) : null}
+        {params.miner ? (
+          <p className={classes.address}>
+            <span className={classes.span}>Blocks mined by</span>{" "}
+            <FusionAddressLink address={params.miner} />
+          </p>
+        ) : null}
+        <MinedBlocks
+          miner={params.miner}
+          tableOptions={{ pageSize: 10, pageSizeOptions: [10, 20, 50] }}
+        />
+      </Panel>
     </>
-  )
+  );
 }
 
-BlockListPage.getInitialProps = async ({query}) => {
+BlockListPage.getInitialProps = async ({ query }) => {
   return {
-    params: query
-  }
-}
+    params: query,
+  };
+};
