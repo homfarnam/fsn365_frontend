3daf1c2304f7f5fd395b98e70e63c3fa80f348f7
3cats <cat.katecc@gmail.com>
2020-04-02 15:03:55 -0400

    map txns types

--- pages/txs.jsx
+++ pages/txs.jsx
@@ -4,7 +4,8 @@ import Panel from "../src/components/Panel";
 import PageHeading from "../src/components/PageHeading";
 import FusionAddressLink from "../src/components/FusionAddressLink";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-import TextStrong from '../src/components/TextStrong'
+import TextStrong from "../src/components/TextStrong";
+import { TXN_TYPES_MAP } from "../src/constants/txnType";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -12,24 +13,22 @@ const useStyles = makeStyles(() =>
       display: "flex",
       flexWrap: "wrap",
       wordBreak: "break-all",
-      paddingLeft: ".75rem"
+      paddingLeft: ".75rem",
     },
     span: {
       margin: "0 4px",
-      display: "inline-block"
+      display: "inline-block",
     },
     strong: {
-      margin: " 0 .25rem"
-    }
+      margin: " 0 .25rem",
+    },
   })
 );
 
 export default function TxListPage({ params }) {
-  let type = params.type
-    ? `#${params.type.replace("Ext", "").replace("Func", "")}`
-    : "";
-  const { from, to } = params;
-  const hasAddressFilter = from || to;
+  let type = params.type ?  "#" + TXN_TYPES_MAP[params.type]: '';
+  const { from, to, address } = params;
+  const hasAddressFilter = from || to || address;
   const classes = useStyles();
   return (
     <>
@@ -37,9 +36,12 @@ export default function TxListPage({ params }) {
       <Panel style={{ paddingTop: 0, paddingLeft: 0, paddingRight: 0 }}>
         {hasAddressFilter ? (
           <p className={classes.hint}>
-            <TextStrong className={classes.strong}>Notice: </TextStrong>We only provide
+            <TextStrong className={classes.strong}>Notice: </TextStrong>We only
+            provide
             <prev className={classes.prev}></prev>
-            <TextStrong className={classes.strong}>10k non-ticket txns </TextStrong>{" "}
+            <TextStrong className={classes.strong}>
+              10k non-ticket txns{" "}
+            </TextStrong>{" "}
             <prev className={classes.prev}></prev>for an address at most.
           </p>
         ) : null}
@@ -57,6 +59,13 @@ export default function TxListPage({ params }) {
             <FusionAddressLink address={to} />
           </p>
         ) : null}
+        {
+          address? <p className={classes.hint}>
+            <TextStrong className={classes.strong}>All none-tikect </TextStrong>
+            <span className={classes.span}>txns for</span>{" "}
+            <FusionAddressLink address={address} />
+          </p>:null
+        }
         <Transactions params={params} />
       </Panel>
     </>
@@ -64,13 +73,13 @@ export default function TxListPage({ params }) {
 }
 
 TxListPage.getInitialProps = async ({ query }) => {
-  if(query.from) {
+  if (query.from) {
     query.from = query.from.toLowerCase();
   }
-  if(query.to) {
+  if (query.to) {
     query.to = query.to.toLowerCase();
   }
   return {
-    params: query
+    params: query,
   };
 };
---
+++ src/constants/txnType.js
@@ -0,0 +1,12 @@
+export const TXN_TYPES_MAP = {
+  2: "GenAsset",
+  3: "SendAsset",
+  4: "TimeLock",
+  5: "MakeSwap",
+  6: "TakeSwap",
+  7: "RecallSwap",
+  8: "MakeMultiSwap",
+  9: "TakeMultiSwap",
+  10: "RecallMultiSwap",
+  11: "AssetValueChange"
+};
