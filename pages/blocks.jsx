585e594b11f629c249b033c7b81253b20cd63bab
3cats <cat.katecc@gmail.com>
2020-04-28 20:33:51 -0400

    rm miner support

--- pages/blocks.jsx
+++ pages/blocks.jsx
@@ -2,53 +2,17 @@ import React from "react";
 import Panel from "../src/components/Panel";
 import PageHeading from "../src/components/PageHeading";
 import MinedBlocks from "../src/components/MinedBlocks";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
-import FusionAddressLink from "../src/components/FusionAddressLink";
-import TextStrong from "../src/components/TextStrong";
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    address: {
-      display: "flex",
-      flexWrap: "wrap",
-      wordBreak: "break-all",
-      paddingLeft: ".75rem",
-    },
-    span: {
-      margin: "0 4px",
-      display: "inline-block",
-    },
-    panel: {
-      padding: "0",
-    },
-  })
-);
-
-export default function BlockListPage({ miner }) {
-  const style = useStyles();
+export default function BlockListPage(props) {
   return (
     <>
       <PageHeading
         title={"Blocks"}
         canonical={'blocks'}
       />
-      <Panel className={style.panel}>
-        {miner ? (
-          <p p className={style.address}>
-            <TextStrong>Notice:</TextStrong>We only provide latest 1k blocks
-            mined by miner.
-          </p>
-        ) : null}
-        {miner ? (
-          <p className={style.address}>
-            <span className={style.span}>Blocks mined by</span>{" "}
-            <FusionAddressLink address={miner} />
-          </p>
-        ) : null}
-        <MinedBlocks miner={miner} />
+      <Panel style={{padding: 0}}>
+        <MinedBlocks/>
       </Panel>
     </>
   );
 }
-
-BlockListPage.getInitialProps = async ({ query }) => query;
