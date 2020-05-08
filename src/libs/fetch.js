b00226f26691d4feab168b412acef085d592d091
3cats <cat.katecc@gmail.com>
2020-05-08 09:26:39 -0400

    turn off api gateway

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -1,35 +1,34 @@
 import fetch from "isomorphic-fetch";
-import promise from "es6-promise";
 import getConfig from "next/config";
-
-promise.polyfill();
 
 export default function getFetch(path, params = {}) {
   const { publicRuntimeConfig } = getConfig();
   const apiUrl = publicRuntimeConfig.API_PATH + path;
   const url = apiUrl + toQueryString(params);
-  return fetch(url, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    }
-  });
+  console.log(url);
+  return fetch(url);
 }
 
 function toQueryString(params) {
+  let size = 0;
   Object.keys(params).map(key => {
     if (!params[key]) {
       delete params[key];
+    } else {
+      size++;
     }
   });
-  return (
-    "?" +
-    Object.keys(params)
-      .map(function(key) {
-        return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
-      })
-      .join("&")
-  );
+  if (size) {
+    return (
+      "?" +
+      Object.keys(params)
+        .map(function(key) {
+          return (
+            encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
+          );
+        })
+        .join("&")
+    );
+  }
+  return "";
 }
