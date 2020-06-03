a1a81da50600095506e60e96da28b75a9fd4bce0
kate <cat.katecc@gmail.com>
2020-06-03 17:21:57 -0400

    config fetch

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -1,34 +1,19 @@
-import fetch from "isomorphic-fetch";
-import getConfig from "next/config";
+import axios from "axios";
 
-export default function getFetch(path, params = {}) {
-  const { publicRuntimeConfig } = getConfig();
-  const apiUrl = publicRuntimeConfig.API_PATH + path;
-  const url = apiUrl + toQueryString(params);
-  console.log(url);
-  return fetch(url);
-}
-
-function toQueryString(params) {
-  let size = 0;
-  Object.keys(params).map(key => {
-    if (!params[key]) {
-      delete params[key];
-    } else {
-      size++;
-    }
-  });
-  if (size) {
-    return (
-      "?" +
-      Object.keys(params)
-        .map(function(key) {
-          return (
-            encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
-          );
-        })
-        .join("&")
-    );
+axios.defaults.baseURL = "https://api.fsn365.com/";
+axios.interceptors.request.use(config => {
+  config.headers["Content-Type"] = "application/json";
+  return config;
+});
+axios.interceptors.response.use(
+  response => {
+    const { data } = response.data || {};
+    return data || {};
+  },
+  error => {
+    console.debug("error:", error);
+    return {};
   }
-  return "";
-}
+);
+
+export default axios;
